f51a08969cd8147328f02503cd5fb437d40a38c5
Agata Cristina <agata.francisco@portalsesisp.org.br>
2024-06-11 16:46:10 -0300

    atualização Fragments

--- app/src/main/java/com/programardores/safeguardpro/view/InicioFragment.kt
+++ app/src/main/java/com/programardores/safeguardpro/view/InicioFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.programardores.safeguardpro.R
 import com.programardores.safeguardpro.databinding.FragmentCadastroEpiBinding
 import com.programardores.safeguardpro.databinding.FragmentInicioBinding
@@ -13,7 +14,6 @@ import com.programardores.safeguardpro.databinding.FragmentInicioBinding
 class InicioFragment : Fragment() {
     private var _binding: FragmentInicioBinding? = null
     private val binding: FragmentInicioBinding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
