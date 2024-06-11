e358b33c0ddf6b65062ed7bc3e64ce1b6634f556
Agata Cristina <agata.francisco@portalsesisp.org.br>
2024-06-11 08:15:03 -0300

    atualização com o crud

---
+++ app/src/main/java/com/programardores/safeguardpro/service/repository/remote/EntregaService.kt
@@ -0,0 +1,40 @@
+package com.programardores.safeguardpro.service.repository.remote
+
+import com.programardores.safeguardpro.service.model.Entrega
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Part
+import retrofit2.http.Path
+
+interface EntregaService {
+    @GET("selectEntrega")
+    suspend fun getEntregas(): List<Entrega>
+
+    @Multipart
+    @POST("addEntrega")
+    suspend fun createEntrega(
+        @Part("data_entrega") data_entrega: RequestBody,
+        @Part("ca") ca: RequestBody,
+        @Part("periodo") periodo: RequestBody
+    ): Response<Entrega>
+
+    @GET("getEntrega/{entrega_id}")
+    suspend fun getEntregaById(@Path("entrega_id") id: Int): Response<List<Entrega>>
+
+    @Multipart
+    @PUT("updateEntrega/{entrega_id}")
+    suspend fun updateEntrega(
+        @Path("entrega_id") entregaId: Int,
+        @Part("data_entrega") data_entrega: RequestBody,
+        @Part("ca") ca: RequestBody,
+        @Part("periodo") periodo: RequestBody
+    ): Response<Entrega>
+
+    @DELETE("deleteEntrega/{entrega_id}")
+    suspend fun deleteEntregaById(@Path("entrega_id") id: Int): Response<Entrega>
+}
