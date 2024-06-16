e80e9be74ec2603645dfc6f220d222e532d767f1
lukninja97 <lucassilvasouto@gmail.com>
2024-06-16 16:24:41 -0300

    Ajustes nos relatorios, cadastros.
    Logica do login para Supervisor e funcionario.

--- app/src/main/java/com/programardores/safeguardpro/view/FuncionarioCadastroFragment.kt
+++ app/src/main/java/com/programardores/safeguardpro/view/FuncionarioCadastroFragment.kt
@@ -2,6 +2,7 @@ package com.programardores.safeguardpro.view
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,60 +33,76 @@ class FuncionarioCadastroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Carregar o funcionario caso tenha selecionado
+        // 1 - Carregar o funcionario caso tenha selecionado
         arguments?.let {
             viewModel.getFuncionario(it.getInt("funcionarioId"))
         }
 
+        // 2 - Click do botao cadastrar
         binding.btnCadastrar.setOnClickListener {
+            // 3 - Coletar os dados dos edits
             var nome = binding.edtNome.editableText.toString()
             var cpf = binding.edtCpf.editableText.toString()
             var senha = binding.edtSenha.editableText.toString()
 
+            // 4 - Verificar se nao sao vazios
             if (nome != "" && cpf != "" && senha != "") {
+
+                // 5 - preencher os dados da classe
                 val funcionario = Funcionario(
                     nome = nome,
                     cpf = cpf,
                     senha = senha
                 )
 
+                // 6 - Decidir se vai atualizar ou criar
                 viewModel.funcionario.value?.let {
                     funcionario.id = it.id
                     viewModel.update(funcionario)
 
                 } ?: run {
                     viewModel.insert(funcionario)
-
-                    binding.edtNome.editableText.clear()
-                    binding.edtCpf.editableText.clear()
-                    binding.edtSenha.editableText.clear()
-                    findNavController().navigateUp()
                 }
+
+                // 7 - Limpar os edits
+                binding.edtNome.editableText.clear()
+                binding.edtCpf.editableText.clear()
+                binding.edtSenha.editableText.clear()
+                findNavController().navigateUp()
             } else {
                 Toast.makeText(requireContext(), "Digite os dados", Toast.LENGTH_LONG).show()
             }
+        }
 
+        // 9 - Click do botao deletar
+        binding.btnDeletar.setOnClickListener {
+            AlertDialog.Builder(requireContext())
+                .setTitle("Exclusão de funcionário")
+                .setMessage("Você realmente deseja excluir?")
+                .setPositiveButton("Sim") { _, _ ->
+                    viewModel.delete(viewModel.funcionario.value?.id ?: 0)
+                    findNavController().navigateUp()
+                }
+                .setNegativeButton("Não") { _, _ -> }
+                .show()
+        }
 
+        // 10 - Observar a variavel da viewmodel
+        viewModel.funcionario.observe(viewLifecycleOwner) { funcionario ->
 
-            binding.btnDeletar.setOnClickListener {
-                AlertDialog.Builder(requireContext())
-                    .setTitle("Exclusão de funcionário")
-                    .setMessage("Você realmente deseja excluir?")
-                    .setPositiveButton("Sim") { _, _ ->
-                        viewModel.delete(viewModel.funcionario.value?.id ?: 0)
-                        findNavController().navigateUp()
-                    }
-                    .setNegativeButton("Não") { _, _ -> }
-                    .show()
-            }
+            // 11 - Popular os edits
+            binding.edtNome.setText(funcionario.nome)
+            binding.edtCpf.setText(funcionario.cpf)
+            binding.edtSenha.setText(funcionario.senha)
 
-            viewModel.funcionario.observe(viewLifecycleOwner) { funcionario ->
-                binding.edtNome.setText(funcionario.nome)
-                binding.edtCpf.setText(funcionario.cpf)
-                binding.edtSenha.setText(funcionario.senha)
+            // 12 - Mostrar o botao de excluir
+            binding.btnDeletar.visibility = View.VISIBLE
+        }
 
-                binding.btnDeletar.visibility = View.VISIBLE
-            }
+        // 13 - Observar a variavel da viewmodel para mostrar os erros
+        viewModel.erro.observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), "Erro $it", Toast.LENGTH_LONG).show()
+            Log.e("erro Emprestimo", it)
         }
     }
 }
